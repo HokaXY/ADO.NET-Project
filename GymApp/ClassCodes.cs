ccb5a49e1d9329d50a816ede6e8be79f28331303
HokaXY <saba.beridze.2003@gmail.com>
2023-10-05 15:09:55 +0400

    Second week update

---
+++ GymApp/ClassCodes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp
+{
+    class ClassCodes
+    {
+        // we counct How many ID we have in total in selected* table
+        /*static void Main(string[] args)
+        {
+            string conString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+
+            con.Open();
+            string countQuery = "select count (MemberID) from members";
+            using (SqlCommand cmd1 = new SqlCommand(countQuery, con))
+            {
+                int count = (int)cmd1.ExecuteScalar();
+                Console.WriteLine(count);
+            }
+
+        }*/
+
+        //tables infos ganaxleba ubralod sql injection ro aviridot tavidan
+        /*
+        static void Main(string[] args)
+        {
+            string conString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+
+            con.Open();
+
+            string insertQuery = "INSERT INTO [dbo]. [members]([firstName],[LastName])Values(@firstName,@lastName)";
+
+            using (SqlCommand cmd1 = new SqlCommand(insertQuery, con))
+            {
+                cmd1.Parameters.Add("@firstName", SqlDbType.VarChar);
+                cmd1.Parameters.Add("@LastName", SqlDbType.VarChar);
+                cmd1.Parameters["@firstName"].Value = ";DELETE FROM Student;";
+                cmd1.Parameters["@LastName"].Value = "ketelauri2";
+
+                cmd1.ExecuteNonQuery();
+                */
+        /*static void Main(string[] args)
+{
+    string conString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
+    SqlConnection con = new SqlConnection(conString);
+    con.Open();
+    string queryString = "select * from Members";
+    SqlCommand cmd = new SqlCommand(queryString, con);
+    SqlDataReader reader = cmd.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine(reader[0].ToString()+":" + reader[1].ToString()+":" +reader[2].ToString());
+    }
+
+    Console.ReadKey();
+}*/
+    }
+}
--- GymApp/Program.cs
+++ GymApp/Program.cs
@@ -2,30 +2,56 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GymApp
+
 {
     class Program
     {
-        /*static void Main(string[] args)
-        {
-            string conString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
-            SqlConnection con = new SqlConnection(conString);
-            con.Open();
-            string queryString = "select * from Members";
-            SqlCommand cmd = new SqlCommand(queryString, con);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine(reader[0].ToString()+":" + reader[1].ToString()+":" +reader[2].ToString());
-            }
-
-            Console.ReadKey();
-        }*/
+        static readonly string connectionString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
 
         static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Create Product");
+                Console.WriteLine("2. Read Products");
+                Console.WriteLine("3. Update Product");
+                Console.WriteLine("4. Delete Product");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateProduct();
+                        break;
+                    case "2":
+                        ReadProducts();
+                        break;
+                    case "3":
+                        UpdateProduct();
+                        break;
+                    case "4":
+                        DeleteProduct();
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void UpdateProduct()
         {
             string conString = @"Data Source =SABA\SQLEXPRESS; Initial Catalog=GymDB; Integrated Security=True";
 
@@ -64,7 +90,7 @@ namespace GymApp
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("error: "+ ex.Message);
+                    Console.WriteLine("error: " + ex.Message);
                 }
             }
         }
@@ -97,7 +123,124 @@ namespace GymApp
 
                 cmd.ExecuteNonQuery();
             }
-
         }
+        static void CreateProduct()
+        {
+            Console.Write("Enter Product Name: ");
+            string productName = Console.ReadLine();
+
+            Console.Write("Enter Category ID: ");
+            int categoryId = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Price: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO products (Name, category_ID, Price) " +
+                                         "VALUES (@ProductName, @CategoryId, @Price)";
+
+                    using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                    {
+                        insertCommand.Parameters.AddWithValue("@ProductName", productName);
+                        insertCommand.Parameters.AddWithValue("@CategoryId", categoryId);
+                        insertCommand.Parameters.AddWithValue("@Price", price);
+
+                        int rowsAffected = insertCommand.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Product created successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to create the product.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        static void ReadProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string sqlQuery = "SELECT p.Name AS ProductName, c.Name AS CategoryName, p.Price " +
+                                      "FROM products p " +
+                                      "INNER JOIN categories c ON p.category_ID = c.ID";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string productName = (string)reader["ProductName"];
+                                string categoryName = (string)reader["CategoryName"];
+                                decimal price = (decimal)reader["Price"];
+
+                                string rowData = $"Product Name: {productName}, Category Name: {categoryName}, Price: {price:C}";
+                                Console.WriteLine(rowData);
+                            }
+                        }
+                    }
+
+                    connection.Close();
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+
+
+        static void DeleteProduct()
+        {
+            Console.Write("Enter Product ID to delete: ");
+            int productId = int.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string deleteQuery = "DELETE FROM products WHERE ID = @ProductId";
+
+                    using (SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection))
+                    {
+                        deleteCommand.Parameters.AddWithValue("@ProductId", productId);
+
+                        int rowsAffected = deleteCommand.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Product deleted successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No product with the given ID found.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+
     }
 }
